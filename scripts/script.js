babb178a58a0282c795ff9733fb507c1c5f35232
Pavel Prokofev <kirgadan@yandex.ru>
2023-11-25 19:04:22 +0300

    и вновь продолжается бой, начало поклодено

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,13 @@ const themeButtons = document.querySelectorAll('.header__theme-menu-button');
 
 themeButtons.forEach((button) => {
   button.addEventListener('click', () => {
+
     themeButtons.forEach((btn) => {
       btn.classList.remove('header__theme-menu-button_active');
       btn.removeAttribute('disabled');
     });
+
+
     if (
       [...button.classList].includes('header__theme-menu-button_type_light')
     ) {
@@ -17,8 +20,10 @@ themeButtons.forEach((button) => {
     } else {
       changeTheme('auto');
     }
+    
     button.classList.add('header__theme-menu-button_active');
     button.setAttribute('disabled', true);
+
   });
 });
 
